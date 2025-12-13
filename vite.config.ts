b0ffce3871b4faa55f2c22b74498230df202e838
Mehdi Zitouni <mehdi.zitouni1@gmail.com>
2025-12-13 10:45:00 +0100

    Fix: Update base path in vite.config.ts for correct deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  base: "/les-ptits-trinquat-web-main/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
