30da23bc4d6233d33bb696f5fee1a08996cda671
Mehdi Zitouni <mehdi.zitouni1@gmail.com>
2025-12-13 11:32:26 +0100

    � Fix React Router 404 issue on GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Gère la redirection depuis 404.html
+if (sessionStorage.redirect) {
+  const redirect = sessionStorage.redirect;
+  delete sessionStorage.redirect;
+  window.location.href = redirect;
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
