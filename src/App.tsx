a86182d5a9b8a2d22d90f4a4822c2f1fecb956f6
Mehdi Zitouni <mehdi.zitouni1@gmail.com>
2025-12-21 18:00:24 +0100

    ✨ Add "Notre École" page and update routing in App and Header components

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Evenements from "./pages/Evenements";
 import ComptesRendus from "./pages/ComptesRendus";
 import Partenaires from "./pages/Partenaires";
 import APropos from "./pages/APropos";
+import NotreEcole from "./pages/NotreEcole";
 import Contact from "./pages/Contact";
 import MessageEnvoye from "./pages/MessageEnvoye";
 import NotFound from "./pages/NotFound";
@@ -31,6 +32,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/a-propos" element={<APropos />} />
+          <Route path="/notre-ecole" element={<NotreEcole />} />
           <Route path="/evenements" element={<Evenements />} />
           <Route path="/comptes-rendus" element={<ComptesRendus />} />
           <Route path="/partenaires" element={<Partenaires />} />
