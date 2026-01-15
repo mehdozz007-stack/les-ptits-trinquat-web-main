10e977dc13d0edebea2a98199ab291aa46f1cb86
Mehdi Zitouni <mehdi.zitouni1@gmail.com>
2026-01-15 17:20:26 +0100

    Correction quelques passages

--- src/pages/Partenaires.tsx
+++ src/pages/Partenaires.tsx
@@ -53,7 +53,7 @@ const mainPartners = [
     id: 3,
     name: "Enviform Sport Santé",
     category: "Bien être",
-    description: "coaching sportif et bien-être.",
+    description: "Coaching sportif et bien-être.",
     logo: enviForm,
     website: "https://www.enviform-sport-sante.fr/",
   },
