59df6d289e086593ceaea606e47de675a48789e8
Mehdi Zitouni <mehdi.zitouni1@gmail.com>
2026-03-08 17:59:48 +0100

    Updates

--- assets/page-events-DYZaL9CJ.js
+++
@@ -1,1 +0,0 @@
-import"./vendor-form-DVSTEbio.js";import"./vendor-ui-kKnHl0VM.js";import"./page-index-B8DRJhjg.js";
---
+++ assets/page-events-INMVhBHT.js
@@ -0,0 +1,1 @@
+import"./vendor-form-D5wNekCa.js";import"./vendor-ui-2uHsPxRr.js";import"./page-index-B8sugR7Y.js";
