0bd28b20d05b4ca10a81d7152fe6ababb75d7a78
Mehdi Zitouni <mehdi.zitouni1@gmail.com>
2026-03-08 04:40:41 +0100

    Force Cloudflare Pages rebuild - fix React imports for Framer Motion

--- src/main.tsx
+++ src/main.tsx
@@ -4,3 +4,4 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
 // Force rebuild at Fri, Feb 13, 2026  8:51:04 PM
+// Force rebuild at Sun, Mar  8, 2026  4:40:41 AM
